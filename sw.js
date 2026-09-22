c835f67920e1f8ab1c621ff9aaec4f9a41f9ac59
m-tkkm <p5hftg9497@privaterelay.appleid.com>
2026-09-22 12:16:59 +0900

    GA4イベント計測を追加

--- sw.js
+++ sw.js
@@ -1,34 +1,77 @@
-const CACHE_NAME = "workoutlog-v3";
-const APP_FILES = ["./", "./index.html", "./style.css", "./script.js", "./manifest.webmanifest"];
+const CACHE_NAME = "workoutlog-v4";
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache =>
-            cache.addAll(APP_FILES)
-        )
-    );
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.webmanifest"
+];
 
-    self.skipWaiting();
-});
+self.addEventListener(
+    "install",
+    event => {
+        event.waitUntil(
+            caches
+                .open(
+                    CACHE_NAME
+                )
+                .then(
+                    cache =>
+                        cache.addAll(
+                            APP_FILES
+                        )
+                )
+        );
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        )
-    );
+        self.skipWaiting();
+    }
+);
 
-    self.clients.claim();
-});
+self.addEventListener(
+    "activate",
+    event => {
+        event.waitUntil(
+            caches
+                .keys()
+                .then(
+                    keys =>
+                        Promise.all(
+                            keys
+                                .filter(
+                                    key =>
+                                        key !==
+                                        CACHE_NAME
+                                )
+                                .map(
+                                    key =>
+                                        caches.delete(
+                                            key
+                                        )
+                                )
+                        )
+                )
+        );
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response =>
-            response || fetch(event.request)
-        )
-    );
-});
+        self.clients.claim();
+    }
+);
+
+self.addEventListener(
+    "fetch",
+    event => {
+        event.respondWith(
+            caches
+                .match(
+                    event.request
+                )
+                .then(
+                    response =>
+                        response ||
+                        fetch(
+                            event.request
+                        )
+                )
+        );
+    }
+);
